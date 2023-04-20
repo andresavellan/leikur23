abe7831ef317c6583f0e4321a76776f3d5da8ebd
Andres Avellan <andres.avellan@omegapoint.se>
2023-04-20 23:51:18 +0200

    first commit

---
+++ js/nav.js
@@ -0,0 +1,33 @@
+(function () {
+	const burgerWrapper = document.querySelector(".burger-wrapper");
+	const burger = document.querySelector(".burger");
+	const nav = document.querySelector(".nav-links-burger");
+	const navLinks = document.querySelectorAll(".nav-links-burger li");
+
+	burgerWrapper.addEventListener("click", () => {
+		nav.classList.toggle("nav-active");
+
+		navLinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = "";
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease forwards ${
+					index / 7 + 0.6
+				}s`;
+			}
+		});
+		burgerWrapper.classList.toggle("open");
+		burger.classList.toggle("toggle");
+	});
+
+	navLinks.forEach((link) => {
+		link.addEventListener("click", function () {
+			nav.classList.remove("nav-active");
+			navLinks.forEach((link, index) => {
+				link.style.animation = "";
+			});
+			burgerWrapper.classList.toggle("closed");
+			burger.classList.remove("toggle");
+		});
+	});
+})();
